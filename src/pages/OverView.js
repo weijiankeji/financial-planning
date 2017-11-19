111099fa97ac22c9e53aec1e27f07c1d7e8951bf
封宇 <windyrain1994@163.com>
2017-11-19 21:16:23 +0800

    feat: 添加前言

--- src/pages/OverView.js
+++ src/pages/OverView.js
@@ -4,13 +4,38 @@ import styled from 'styled-components';
 const Container = styled.div`
   display: flex;
   flex: 1;
+  flex-direction: column;
+  background: #e0d9c8;
+`;
+
+const Preface = styled.p`
+  margin: 12px;
+  display: flex;
+  justify-content: center;
+  color: #666;
+`;
+
+const Author = styled.p`
+  margin: 0px;
+  display:flex;
+  justify-content: flex-end;
+  padding-right: 12px;
+  color: #999;
+`;
+
+const Link = styled.a`
+  color: #999;
+  text-decoration: none;
 `;
 
 export class OverView extends Component{
 
   render() {
     return (
-      <Container>OverView</Container>
+      <Container>
+        <Preface>所有人都希望自己变得富有一些，只是有些人的这一愿望更为强烈</Preface>
+        <Author>——— <Link href="https://baike.baidu.com/item/%E5%8D%9A%E5%A4%9A%C2%B7%E8%88%8D%E8%B4%B9%E5%B0%94/9869587?fr=aladdin" target="_blank">Bodo Schaefer</Link></Author>
+      </Container>
     );
   }
 
