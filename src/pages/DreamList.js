3a96113990ba16765aa94e26507bd5c9876703e5
封宇 <windyrain1994@163.com>
2017-12-03 11:55:18 +0800

    feat: 添加组件BriefDreamList，用于显示最近一月的愿望

--- src/pages/DreamList.js
+++ src/pages/DreamList.js
@@ -15,3 +15,36 @@ export class DreamList extends Component{
   }
 
 }
+
+const BriefList = styled.ul`
+  list-style: none;
+	margin:0;
+	padding:1rem 3rem 1rem;
+	max-width:500px;
+	background:#7DB6F8;
+	color:#FFF;
+	box-shadow:-8px -8px 0px 0px rgba(180,180,180,.1);
+`;
+
+const BriefDream = styled.li`
+	display:flex;
+	margin:0 -3rem 4px;
+	padding:1.1rem 3rem;
+	justify-content:space-between;
+	align-items:center;
+	background:rgba(255,255,255,0.1);
+`
+
+export class BriefDreamList extends Component {
+
+  render() {
+    return (
+      <BriefList>
+        <BriefDream>减肥10斤</BriefDream>
+        <BriefDream>去小吊梨汤吃顿饭</BriefDream>
+        <BriefDream>买1件衣服</BriefDream>
+      </BriefList>
+    );
+  }
+
+}
